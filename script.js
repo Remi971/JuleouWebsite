44b60dc2f82310c8a89c41f8a1a1f5e0913a61f3
Remi <remi.bha@gmail.com>
2021-04-10 17:34:58 +0200

    in progress

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ const toggle = document.querySelector('.toggle')
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('active')
 })
+
+var scrollBefore = 0;
+
+window.addEventListener('scroll',function(e){
+    const scrolled = window.scrollY;
+
+    if(scrollBefore > scrolled){
+        console.log("ScrollUP");
+        scrollBefore = scrolled;
+        //Desired action
+    }else{
+        scrollBefore = scrolled;
+        console.log("ScrollDOWN");
+        //Desired action
+    }
+})
+
+const dot = document.querySelectorAll('.dot');
+dot.forEach((item, i) => {
+  item.addEventListener("mouseenter", function() {
+    console.log("it works")
+  })
+});
