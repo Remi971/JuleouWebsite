1e3eb65e95a265ac0b671099591636e3b0059927
Remi <remi.bha@gmail.com>
2021-04-11 15:30:06 +0200

    navigation

--- script.js
+++ script.js
@@ -20,9 +20,30 @@ window.addEventListener('scroll',function(e){
     }
 })
 
-const dot = document.querySelectorAll('.dot');
-dot.forEach((item, i) => {
-  item.addEventListener("mouseenter", function() {
-    console.log("it works")
+const start = document.querySelector(".start");
+start.classList.add("selected")
+
+const part = document.querySelectorAll('.part');
+part.forEach((item, i) => {
+  const line = item.querySelector('.line');
+  const aDot = item.querySelector('.dot');
+  const span = item.querySelector('span');
+  const dot = aDot.querySelector('span');
+  const point = document.querySelectorAll("#point")
+  aDot.addEventListener("mouseenter", function() {
+    line.style.width = '30px';
+    span.style.opacity = 1;
+    dot.classList.add('active');
+  })
+  aDot.addEventListener("mouseleave", function() {
+    line.style.width = '0px';
+    span.style.opacity = 0;
+    dot.classList.remove('active');
+  })
+  aDot.addEventListener("click", function() {
+    point.forEach((el, i) => {
+      el.classList.remove("selected")
+    })
+    dot.classList.add("selected")
   })
 });
