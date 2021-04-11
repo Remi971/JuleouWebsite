e7ed7bafac8f3105ddbf9a49eddc502f246a60d8
Remi <remi.bha@gmail.com>
2021-04-11 20:30:33 +0200

    navigateur et le menu

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-const toggle = document.querySelector('.toggle')
-
-toggle.addEventListener('click', () => {
-  toggle.classList.toggle('active')
-})
-
-var scrollBefore = 0;
-
-window.addEventListener('scroll',function(e){
-    const scrolled = window.scrollY;
-
-    if(scrollBefore > scrolled){
-        console.log("ScrollUP");
-        scrollBefore = scrolled;
-        //Desired action
-    }else{
-        scrollBefore = scrolled;
-        console.log("ScrollDOWN");
-        //Desired action
-    }
-})
-
+//Sections
+const sections = document.querySelectorAll(".section")
+// activation de la puce de la première page
 const start = document.querySelector(".start");
-start.classList.add("selected")
+start.classList.add("selected");
+const accueil = document.querySelector(".part1");
+//accueil.classList.add("selectionActive");
+//navigateur
+const navigateur = document.querySelector(".navigateur")
+//Puces du Navigateur
+const puces = document.querySelectorAll("#point")
 
+//Activation de la puce en fonction de la section affichée
+function checkSection() {
+  sections.forEach((section, i) => {
+    // console.log(section.getBoundingClientRect().top)
+    // console.log(window.innerHeight)
+    if (section.getBoundingClientRect().top > -window.innerHeight/2 && section.getBoundingClientRect().top < window.innerHeight/2){
+      puces[i].classList.add("selected");
+      section.classList.add("sectionActive")
+    } else {
+      puces[i].classList.remove("selected");
+      if (!section.classList.contains("reduit")){
+        section.classList.remove("sectionActive")
+      }
+    }
+  })
+};
+const wrapper = document.querySelector(".wrapper")
+wrapper.addEventListener("scroll", checkSection);
+checkSection();
+
+// Navigateur et ses puces
 const part = document.querySelectorAll('.part');
 part.forEach((item, i) => {
   const line = item.querySelector('.line');
@@ -34,16 +42,29 @@ part.forEach((item, i) => {
     line.style.width = '30px';
     span.style.opacity = 1;
     dot.classList.add('active');
+    console.log()
   })
   aDot.addEventListener("mouseleave", function() {
     line.style.width = '0px';
     span.style.opacity = 0;
     dot.classList.remove('active');
   })
-  aDot.addEventListener("click", function() {
-    point.forEach((el, i) => {
-      el.classList.remove("selected")
-    })
-    dot.classList.add("selected")
-  })
+  // aDot.addEventListener("click", function() {
+  //   point.forEach((el, i) => {
+  //     el.classList.remove("selected")
+  //   })
+  //   dot.classList.add("selected")
+  // })
 });
+
+//Menu
+const toggle = document.querySelector('.toggle')
+const menu = document.getElementById("menu");
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle("active");
+  navigateur.classList.toggle("menu");
+  const sectionActive = document.querySelector(".sectionActive");
+  sectionActive.classList.toggle("reduit");
+  menu.classList.toggle("activated");
+
+})
